0cb69bc6108b0087e460310977b886c324c778a5
P. William Hozier <phozier@umass.edu>
2016-02-14 00:23:33 -0500

    final final version of process_termination.c commited. added error handling and set the system call within while loop.

--- LAB01/process_termination.c
+++ LAB01/process_termination.c
@@ -13,13 +13,13 @@ void interface(){
   // pid_t pid;
   while (1/* condition */) {
     /* code */
-    // read a string, if string is an integer, proceed; else exit 
+    // read a string, if string is an integer, proceed; else exit
     printf("%s: ","Enter a process id [options -- an integer | -- a noninteger to exit ]");
     scanf("%s", &response);
 
     if (isdigit(response)/* condition */) {
       /* code */
-      printf("Killing... %d\n\n", response);
+      printf("Killing... %d\n\n", atoi(&response));
       pid_t pid = (pid_t)(atoi(&response));
       process_termination(pid/*some pid from the command line*/);
     } else { printf("%s\n", "exiting..."); break; }
