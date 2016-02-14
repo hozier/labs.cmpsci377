c74cb1f553a62ca0d73a95311f1e92cf8563290e
P. William Hozier <phozier@umass.edu>
2016-02-13 23:50:59 -0500

    final version of process_termination.c commited.

--- LAB01/process_termination.c
+++ LAB01/process_termination.c
@@ -3,16 +3,26 @@
 #include <unistd.h>
 #include <signal.h>
 
-void process_termination(pid_t pid){ kill(pid, SIGINT); }
+void process_termination(pid_t pid){ kill(pid, SIGKILL); }
+
+// overview: prompt the user to enter a process id and then
+// use the kill system call to terminate it.
+void interface(){
+  int response = 0;
+  // pid_t pid;
+
+  printf("%s: ","Enter a process id [options -- an integer]");
+  scanf("%d", &response);
+  printf("%s\n\n", "Attempting kill...");
+
+  pid_t pid = (pid_t)response;
+  process_termination(pid/*some pid from the command line*/);
+}
+
 
 int main(int argc, char const *argv[]) {
   /* code */
-  const char *user_cli_input = argv[1];
-  pid_t pid = atoi(user_cli_input);
-  printf("%s%d...\n", "Killing ", pid);
-
-  // TODO: prompt the user to enter a process id and then
-  // use the kill system call to terminate it.
-  process_termination(pid/*some pid from the command line*/);
+  // overview: runs the kill routine.
+  interface();
   return 0;
 }
