9a3fc3c7469b248782fc0d4d65d21c8d1f7b13f0
P. William Hozier <phozier@umass.edu>
2016-03-14 01:23:17 -0400

    solution.py ruby->py complete

--- LAB02/Part1/solution.py
+++ LAB02/Part1/solution.py
@@ -1,48 +1,59 @@
 import json
 from collections import deque
-
 f = open('100-100-8.json', 'r') # <------ replace this json file with one of the randomly generated ones......
 data = json.loads(f.read()) # job now contains a dict of the jobs
-waiting_queue = deque(data['jobs'])
-system_time = int(data['simulation_time'])+1
+
+def map_key(x):
+    x['wait_time'] = 0
+    return x
+
+def update_wait_time(x):
+    x['wait_time']+=1
+    return x
+
+waiting_queue = deque([map_key(x) for x in data['jobs']])
+simulation_time = int(data['simulation_time'])
 number_of_jobs = int(data['number_of_jobs'])
+current_jobs = deque()
+finished_jobs = deque()
+
 time_slice = 1
+timer = 0
 
-i = 0
-head = waiting_queue.popleft()
-next_job_starts = int(waiting_queue[0]['start_time'])
-while i < system_time:
-    try:
 
-        if i == next_job_starts:
-            print "i: ", i, "job start_time: ", next_job_starts
-            waiting_queue.append(head)
-            head = waiting_queue.popleft()
-            old_start_time = next_job_starts
-            next_job_starts = int(waiting_queue[0]['start_time'])
+# print waiting_queue.popleft()
+while timer <= simulation_time:
+    if waiting_queue:
+        jobx_start_time = int( waiting_queue[0]['start_time'] )
+        print jobx_start_time
 
-            # while there are multiple jobs to run at the same start_time, do the following
-            while old_start_time == next_job_starts:
-                if int(head['job_length']) == 0:
-                    head = waiting_queue.popleft()
-                else:
-                    head['job_length']= int(head['job_length']) - time_slice
+        if jobx_start_time == timer:
+            current_jobs.append(waiting_queue.popleft())
+            continue
 
-                    # take head off CPU
-                    waiting_queue.append(head)
-
-                    #get new head
-                    head = waiting_queue.popleft()
-                    next_job_starts = int(waiting_queue[0]['start_time'])
-                    print "i: ", i, "job start_time: ", next_job_starts
+    if len(current_jobs) > 0:
+        job_length = int(current_jobs[0]['job_length'])
+        if job_length == 0:
+            finished_jobs.append(current_jobs.popleft())
 
         else:
-            head['job_length']= int(head['job_length']) - time_slice
-            if int(head['job_length']) == 0:
-                head = waiting_queue.popleft()
+            '''time left == the job_length remaining'''
+            job_length -= time_slice
+            current_jobs[0]['job_length'] = job_length
+            print job_length
 
-        # waiting_queue.append(head)
-            print head['job_length']
-    except:
-        pass
-    i += 1
+            for j in range(1,len(current_jobs)):
+                current_jobs[j]['wait_time']+=1
+
+            current_jobs.append(current_jobs.popleft())
+    timer += 1
+
+''' end while loop '''
+total_time = 0
+for job in finished_jobs:
+    total_time += job['wait_time']
+print 'total_time, ', total_time
+print 'number_of_jobs', number_of_jobs
+
+AWT = total_time/float(number_of_jobs)
+print "AWT", AWT
