cad754fbd3d990dd6c5f88f79c7272e95f8c867e
P. William Hozier <phozier@umass.edu>
2016-04-20 06:15:22 -0400

    Finalizing documentation of paging.c and paging.h

--- LAB03/Part1-Paging/paging.c
+++ LAB03/Part1-Paging/paging.c
@@ -1,21 +1,22 @@
 #include "paging.h"
-#include <math.h>
 
-// TODO: needs to be deubugged.
- uint16_t compute_page_number( uint16_t m,  uint16_t n,  uint16_t v){
+// overview: helps map the page number of a given logical address
+uint16_t compute_page_number( uint16_t m,  uint16_t n,  uint16_t v){
   return (v >> ((1 << (n))-2));  // v DIV 2**n
 }
- uint16_t compute_page_offset( uint16_t m,  uint16_t n,  uint16_t v){
+
+// overview: helps map the offset of a given logical address
+uint16_t compute_page_offset( uint16_t m,  uint16_t n,  uint16_t v){
   uint16_t d = (1 << (n)) - 1; // v MOD 2**n
   return v & (d);
 }
 
-void init_data_structure(logical_address **logical_address_array, uint16_t *v, int S){
+// overview: performs the mappings of v. addresses which need to be mapped
+// from to a page number and an offset
+void perform_paging(logical_address **logical_address_array, uint16_t *v, int S){
   // initialize logical add. array
   for (size_t i = 0; i < S; i++) {
     /* code */
-    // printf("ghjkl %d\n", v[i+2]);
-
     logical_address_array[i] = new_logical_address();
     logical_address_array[i]->page_number = compute_page_number(v[1], v[0], v[i+2]);
     logical_address_array[i]->offset =   compute_page_offset(v[1], v[0], v[i+2]);
@@ -23,6 +24,7 @@ void init_data_structure(logical_address **logical_address_array, uint16_t *v, i
   }
 }
 
+// overview: returns the length of the virtual address array to be created
 int SIZE(FILE *fp){
   int lines = 0;
   char ch = '\0';
@@ -33,6 +35,7 @@ int SIZE(FILE *fp){
   return lines;
 }
 
+// overview: parses the virtual addresses from tracefile
 void parse_logical_addresses(FILE *fp, int S, uint16_t l[]){
   int lines = 0;
   int ch;
@@ -42,23 +45,25 @@ void parse_logical_addresses(FILE *fp, int S, uint16_t l[]){
   }
 }
 
-
+// the main driving method.
 int main(int argc, char const *argv[]) {
   /* code */
 
-  FILE *fp;
-
-  fp = fopen("trace", "r");
+  FILE *fp; // file descriptor
+  fp = fopen("trace", "r"); // paging tracefile
   int S = SIZE(fp)-2; // size of logical_address array
-
-  logical_address **logical_address_array = (logical_address **)malloc(sizeof(logical_address) * S); // creates an array of logical_address structs
-  fp = fopen("trace", "r");
-
-
   uint16_t v[S]; // virtual addresses from file
-  parse_logical_addresses(fp, S, v);
 
-  init_data_structure(logical_address_array, v, S);
+  // overview: creates an array of logical_address structs which will store
+  // the computed page numbers and offsets
+  logical_address **logical_address_array = (logical_address **)malloc(sizeof(logical_address) * S);
+
+  // overview: parse trace
+  parse_logical_addresses(fopen("trace", "r"), S, v);
+
+  // overview: complete mappings from v[i] to (p) and (d)
+  // store mappings within the pointer array `logical_address_array`
+  perform_paging(logical_address_array, v, S);
   fclose(fp);
 
   return 0;
